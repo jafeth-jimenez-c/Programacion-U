c8800b3d40220baf74c612e13b7ff38d233afe36
Jafeth Jimenez Carvajal <84998275+jafeth-jimenez-c@users.noreply.github.com>
2021-06-03 15:26:32 -0600

    Primer Commit

---
+++ src/Semana2.java
@@ -0,0 +1,37 @@
+/*lab 2, en este programa se suman dos variables.
+coment*/
+
+public class Semana2 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("El resultado es: ");
+
+        int box1;
+        int num2;
+        int resultado;
+        box1 = 1;
+        num2 = 55;
+        resultado = box1 + num2;
+        
+        //Esta linea imprime al usuario el resultado
+        System.out.println(resultado);
+
+        float salario;
+        salario = 200.55f;
+
+        double resultadosalarial;
+        resultadosalarial = 500.500;
+
+        boolean siSalimosMediaNoche;
+        siSalimosMediaNoche = true;
+
+        String ayerFue;
+        ayerFue = "Si DEMASIADO";
+
+        char tipoSangre;
+        tipoSangre = 'B';
+
+    }
+
+}
