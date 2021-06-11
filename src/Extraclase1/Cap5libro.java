59dc6bd343c3ca40abae2b25edce48d1604cb59c
Jafeth Jimenez Carvajal <84998275+jafeth-jimenez-c@users.noreply.github.com>
2021-06-11 16:56:46 -0600

    Primer Commit

---
+++ src/Extraclase1/Cap5libro.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+public class Cap5libro {
+
+//5.3 libro
+    public static void main(String[] args) {
+// el encabezado de la instrucción for incluye la inicialización,
+// la condición de continuación de ciclo y el incremento
+        for (int contador = 1; contador <= 10; contador++) {
+            System.out.printf(
+                    "%d ", contador);
+        }
+
+        System.out.println();
+    }
+} // fin de la clase ContadorFo
+
+// Fig. 5.5: Suma.java
+// Sumar enteros con la instrucción for.
+public static void (String[] args){ 
+        {
+    int total = 0;
+
+// total de los enteros pares del 2 al 20
+    for (int numero = 2; numero <= 20; numero += 2)
+           total += numero;
+
+    System.out.printf("La suma es %d%n", total); 
+        }
+} // fin de la clase Suma
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/Extraclase1/Extraaaclass.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Extraclase1;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Extraaaclass {
+    public static void main (String []args){
+            
+double solucion = 0;
+solucion = calcularImpuesto (0<200000);
+System.out.println("Dependiente 1" + "" + solucion);
+
+solucion = calcularImpuesto(200000<=400000);
+System.out.println("Dependiente 2" + "" + solucion);
+
+solucion = calcularImpuesto(400000<=600000);
+System.out.println("Dependiente 3" + "" + solucion);
+
+solucion = calcularImpuesto (600000<=800000);
+System.out.println("Dependiente 4" + "" + solucion);
+
+solucion = calcularImpuesto (0>=800000);
+System.out.println("Dependiente 5" + "" + solucion);
+
+
+}
+    public static double calcularImpuesto(double salario) {
+        double  resultado = 0;
+        ret
+    }
+            }
