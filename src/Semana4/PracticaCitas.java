234471323d288fc301d0e088698a5344f395db92
Jafeth Jimenez Carvajal <84998275+jafeth-jimenez-c@users.noreply.github.com>
2021-06-12 17:42:27 -0600

    practica

--- src/Semana4/PracticaCitas.java
+++ src/Semana4/PracticaCitas.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Semana4;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Usuario
- */
+
 public class PracticaCitas {
 
 
@@ -48,8 +41,6 @@ public class PracticaCitas {
         }
         return resultado;
     }
-    public static void main(String[] args){
-        
-    }
+
 }
 
