b1b923a6f7523644ad2c91954179a8e8e203ca08
Jafeth Jimenez Carvajal <84998275+jafeth-jimenez-c@users.noreply.github.com>
2021-06-11 21:15:20 -0600

    Primer Commit

---
+++ src/Semana4/practica.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Semana4;
+
+/**
+ *
+ * @author Usuario
+ */
+public class practica {
+ //public static void main(String[] args){
+//    practica z = new practica();
+ 
+  public String conmayoriesgo(int numRiegos) {
+     String resultado="";
+    
+   if(numRiegos == 1){
+       resultado = "1 semana";
+      }
+   else if(numRiegos== 2){
+       resultado = "3 dias";
+   }
+   if(numRiegos== 3){
+       resultado = "dia siguiente";
+               }
+   
+   
+     return resultado;
+     
+     
+     
+        }
+  }
+ 
