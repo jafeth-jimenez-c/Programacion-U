9ef3795e94eebb0f2e69fa75cc6c2f2b793d7338
Jafeth Jimenez Carvajal <84998275+jafeth-jimenez-c@users.noreply.github.com>
2021-06-18 15:17:11 -0600

    practica

--- src/Semana4/Tester.java
+++ src/Semana4/Tester.java
@@ -1,18 +1,15 @@
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package semana4;
 
-
+/**
+ *
+ * @author Usuario
+ */
 public class Tester {
     public static void main(String[] args) {
         SolucionIR4 s = new SolucionIR4();
-        double r = s.calcularImpuesto(1000000, 1);
+        double r = s.calcularImpuesto(1, 200000);
         System.out.println(r);
-
         
     }
 }
