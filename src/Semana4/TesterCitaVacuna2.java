e3391579994d8a39dc4af427283edc76953f9574
Jafeth Jimenez Carvajal <84998275+jafeth-jimenez-c@users.noreply.github.com>
2021-06-18 16:58:52 -0600

    practica

--- src/Semana4/TesterCitaVacuna2.java
+++ src/Semana4/TesterCitaVacuna2.java
@@ -4,7 +4,7 @@ package semana4;
 public class TesterCitaVacuna2 {
     public static void main(String[] args) {
         SolucionCitaVacuna2 solucion  = new SolucionCitaVacuna2();
-
+        short nFactores = 1;
         String mensaje;
                 
         mensaje = solucion.obtenerCitaVacuna();
