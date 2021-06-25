02387376542ea1b6b5acf9f6025096233fd8897b
Jafeth Jimenez Carvajal <84998275+jafeth-jimenez-c@users.noreply.github.com>
2021-06-25 16:58:12 -0600

    practica

---
+++ src/EjerciciosDeExamen/Problema8.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EjerciciosDeExamen;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Problema8 {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int dolar1 = 0;
+        int dolar2 = 0;
+        int dolar3 = 0;
+        int dolar4 = 0;
+        int dolar5 = 0;
+        int dolar6 = 0;
+        int monto;        
+                while(true){
+                    System.out.println("Dolares \n1" + "Digitar monto \n2)");
+                   int opcion = s.nextInt();
+                   
+                   switch(opcion){
+                       
+                       case 1:{
+                           if(dolar1==1){
+                          
+                           }
+                           if(dolar2==5){                         
+                       }
+                           if(dolar3==10){
+                           }
+                           if(dolar4==100){
+                           }
+                           if(dolar5==500){
+                           }
+                           if(dolar6==1000){                             
+                           }
+                       break;
+                   }
+                       case 2:{
+                           break;
+                       }
+                   }
+                }
+    }
+}
