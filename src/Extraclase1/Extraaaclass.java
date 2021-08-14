6da9d9c6cfa8f1bdd45c33af5cba0691f67e9249
Jafeth Jimenez Carvajal <84998275+jafeth-jimenez-c@users.noreply.github.com>
2021-08-13 18:27:48 -0600

    Semana 8

--- src/Extraclase1/Extraaaclass.java
+++ src/Extraclase1/Extraaaclass.java
@@ -10,28 +10,29 @@ package Extraclase1;
  * @author Usuario
  */
 public class Extraaaclass {
-    public static void main (String []args){
-            
-double solucion = 0;
-solucion = calcularImpuesto (0<200000);
-System.out.println("Dependiente 1" + "" + solucion);
 
-solucion = calcularImpuesto(200000<=400000);
-System.out.println("Dependiente 2" + "" + solucion);
-
-solucion = calcularImpuesto(400000<=600000);
-System.out.println("Dependiente 3" + "" + solucion);
-
-solucion = calcularImpuesto (600000<=800000);
-System.out.println("Dependiente 4" + "" + solucion);
-
-solucion = calcularImpuesto (0>=800000);
-System.out.println("Dependiente 5" + "" + solucion);
-
-
-}
     public static double calcularImpuesto(double salario) {
-        double  resultado = 0;
-        ret
+        double resultado = 0;
+        return  0;}}
+
+    
+/*
+        double solucion = 0;
+        solucion = calcularImpuesto(solucion <= 200000);
+        System.out.println("Dependiente 1" + "" + solucion);
+
+        solucion = calcularImpuesto(200000 < 400000);
+        System.out.println("Dependiente 2" + "" + solucion);
+
+        solucion = calcularImpuesto(400000 < 600000);
+        System.out.println("Dependiente 3" + "" + solucion);
+
+        solucion = calcularImpuesto(600000 < 800000);
+        System.out.println("Dependiente 4" + "" + solucion);
+
+        solucion = calcularImpuesto( <= 800000);
+        System.out.println("Dependiente 5" + "" + solucion);
+
     }
-            }
+*/
+ 
