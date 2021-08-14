511a6919ea8bce2c8951d71da46ee0465f47602c
Jafeth Jimenez Carvajal <84998275+jafeth-jimenez-c@users.noreply.github.com>
2021-08-13 22:02:02 -0600

    Semana 13

---
+++ src/Semana13/NotaControlador.java
@@ -0,0 +1,22 @@
+package Semana13;
+
+public class NotaControlador {
+    boolean modoGrafico =true;
+    public void iniciar() {
+        Nota nota = new Nota(2);
+        NotaVista nv = new NotaVista(nota,modoGrafico);
+        int op;
+        do {
+            op = nv.getOpcion();
+            switch (op) {
+                case 1:
+                    nv.leaNota();
+                    break;
+                case 2:
+                    nv.muestreNotas();
+                    break;
+                default:
+            }
+        } while (op != 3);
+    }
+}
---
+++ src/Semana13/Tester.java
@@ -0,0 +1,12 @@
+package Semana13;
+
+/**
+ *
+ * @author carlosandres.mendez
+ */
+public class Tester {
+    public static void main(String[] args) {
+        NotaControlador c = new NotaControlador();
+        c.iniciar();
+    }
+}
